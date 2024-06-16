3764a61da22f1b05ff725f7c557ce68e25d4ed5d
mikhailkolesnikov <mikhail.kolesnikov@outlook.de>
2024-06-16 23:09:56 +0200

    add days

---
+++ src/HomeworkNumbers.java
@@ -0,0 +1,65 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HomeworkNumbers {
+    public static void main(String[] args) {
+
+        Set<Integer> set = new HashSet<>(1, 5);
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+
+
+
+
+        }
+        Integer first = scanner.nextInt();
+        Integer second = scanner.nextInt();
+        Integer third = scanner.nextInt();
+        Integer four = scanner.nextInt();
+        Integer five = scanner.nextInt();
+
+
+
+       while (true) {
+
+           i
+           }
+
+
+
+
+            }
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+//  int[] array = new int[7];
+//        Scanner scanner = new Scanner(System.in);
+//for (int i = 0; i < array.length; i++) {
+//            array[i] = scanner.nextInt();
+
+//et<String> set = new HashSet<>();
+//
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            String string = scanner.nextLine();
+//            if (set.contains(string)) {
+//                System.out.println("Повторный ввод");
+//            } else {
+//                set.add(string);
+//
+//                System.out.println("Ok");
+//            }
+
+////1. Вводится 5 чисел. Напишите "Yes", если они не все равны между собой.
+////    1.1 Нужно использовать Set
+////    1.2 Запрещено писать в if условие (1 == 2 и 2 == 3 и 3 == 4), contains использовать нельзя
